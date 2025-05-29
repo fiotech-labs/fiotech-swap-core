178148cd7dbb6d30067e9793ec947f0fdf3ae424
Nolan Le <nolanle898@gmail.com>
2025-05-29 18:14:12 +0700

    update networks config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,11 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-ignition";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const { BNW_RPC_URL, BNW_TESTNET_RPC_URL, PRIVATE_KEY } = process.env;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
@@ -9,7 +14,16 @@ const config: HardhatUserConfig = {
       allowUnlimitedContractSize: false,
     },
     bnw: {
-      url: `http://174.138.18.77:8545`,
+      url: BNW_TESTNET_RPC_URL || "",
+      chainId: 999,
+      accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
+      gasPrice: 5_000_000_000,
+    },
+    bnwTestnet: {
+      url: BNW_TESTNET_RPC_URL || "",
+      chainId: 714,
+      accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
+      gasPrice: 5_000_000_000,
     },
   },
 };
