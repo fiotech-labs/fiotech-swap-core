54bd702be625928c0e1917836aa83b9432f564cd
Nolan Le <nolanle898@gmail.com>
2025-05-29 22:46:54 +0700

    update hardhat networks

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,25 +5,17 @@ import * as dotenv from "dotenv";
 
 dotenv.config();
 
-const { BNW_RPC_URL, BNW_TESTNET_RPC_URL, PRIVATE_KEY } = process.env;
+const { BNW_RPC_URL, PRIVATE_KEY } = process.env;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
   networks: {
-    hardhat: {
-      allowUnlimitedContractSize: false,
-    },
-    bnw: {
+    mainnet: {
       url: BNW_RPC_URL || "",
-      chainId: 999,
-      accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
-      gasPrice: 5_000_000_000,
-    },
-    bnwTestnet: {
-      url: BNW_TESTNET_RPC_URL || "",
       chainId: 714,
       accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
-      gasPrice: 5_000_000_000,
+      initialBaseFeePerGas: 0,
+      gasPrice: 10_000_000_000,
     },
   },
 };
